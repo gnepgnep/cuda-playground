8977ff267d33002b5acefaf151acf4165841ecea
pengpeng <pumbaa1616@163.com>
2024-09-08 13:49:01 +0800

    update comments

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # torchcuda
+
+## cuda practice project
+
+- base: common class
+    - GPUTensor
+    - Logger
+- gpu_properties: check gpu info
+- ops: cuda kernel def
+    - vec_add
+    - add_bias_relu
+    - transpose
+    - reduce_sum
+- test: test kernel consistency and time
--- ops/reduce_sum.cu
+++ ops/reduce_sum.cu
@@ -1,12 +1,30 @@
+/*
+reduce_kernel1: origin implemetation
+reduce_kernel2: reindex tid so that all thread in each wrap is working
+reduce_kernel3: reduce bank conflict
+reduce_kernel4: reduce shared memory, eliminate thread that only read data
+reduce_kernel5: unravel the loop for the last warp, forceinline avoid shared memory cache
+reduce_kernel6: use warp sync function
+reduce_kernel7: reduce dim > 1024
+*/
+
 #pragma once
 #include <iostream>
 
-extern __shared__ float dynamic_shared_data[];
+inline int CalcBlockSize(const in N) {
+    int block_size = 32;
+    while (block_size < N && block_size < 1024) {
+        block_size <= 1;
+    }
+    return block_size
+}
+
+extern __shared__ dynamic_shared_buff[];
 template <typename T>
 __global__ void ReduceKernel1(const T *x, T *y, const int M, const int N) {
     int tid = threadIdx.x;
     int mi = blockIdx.x;
-    float *data = dynamic_shared_data;
+    float *data = dynamic_shared_buff;
     data[tid] = tid < N ? (float)x[mi * N + tid]: 0.0f;
     __syncthreads();
     for (int s = 1; s < blockDim.x; s*=2) {
